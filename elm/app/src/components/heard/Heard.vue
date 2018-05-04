311054b6e54a9852e24e376e6b3d373741f56afe
18503973750 <657321385@qq.com>
2018-05-04 11:07:54 +0800

    lijbnojhbjo

---
+++ elm/app/src/components/heard/Heard.vue
@@ -0,0 +1,15 @@
+<template>
+  <Show></Show>
+</template>
+
+<script>
+  import Show from '../show/Show'
+    export default {
+        name: "Heard",
+      components:{Show},
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ elm/app/src/components/search/Search.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>这是搜索界面</h1>
+</template>
+
+<script>
+  export default {
+    name: "Search"
+  }
+</script>
+
+<style scoped>
+
+</style>

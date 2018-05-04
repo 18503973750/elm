2e04aa3b74688f91d177d6b1d51a2dfe344dff6b
MiaoMiaoDeGit <m18838959446@163.com>
2018-05-04 17:26:28 +0800

    252592

--- elm/app/src/components/profile/Profile.vue
+++ elm/app/src/components/profile/Profile.vue
@@ -1,5 +1,23 @@
 <template>
-    <h1>这是我的界面</h1>
+    <div class="out">
+        <div class="top">
+            <div class="top_one">
+                <a href=""><</a>
+                <span>我的</span>
+            </div>
+            <div class="top_two">
+
+            </div>
+            <div class="top_three">
+
+            </div>
+        </div>
+        <div class="middle"></div>
+        <div class="bottom"></div>
+
+
+    </div>
+
 </template>
 
 <script>
@@ -9,5 +27,11 @@
 </script>
 
 <style scoped>
-
+    .top {
+        background:#3190e8;
+    }
+.top_one a{
+    color: red;
+    
+}
 </style>

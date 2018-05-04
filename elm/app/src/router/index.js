66cbabbabda68d639b0e62d788ea4474e8640d28
18503973750 <657321385@qq.com>
2018-05-04 16:55:30 +0800

    no message

--- elm/app/src/router/index.js
+++ elm/app/src/router/index.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 import Heard from '@/components/heard/Heard'
-import Show from '@/components/show/Show'
+
 import Search from '@/components/search/Search'
 import Order from '@/components/order/Order'
 import Profile from '@/components/profile/Profile'
+import  Waimai from '@/components/waimai/Waimai'
 Vue.use(Router)
 
 export default new Router({
@@ -13,29 +14,32 @@ export default new Router({
     {
       path: '/',
       name: 'Heard',
-      component: Heard
-    },
-      {
-          path: '/',
-          name: 'Show',
-          component: Show
-      },
-      {
-          path: '/search',
-          name: 'Search',
-          component: Search
-      },
-      {
-          path: '/order',
-          name: 'Order',
-          component: Order
-      },
+      component: Heard,
+        children:[
+       {
+            path: '/',
+            name: 'Waimai',
+            component: Waimai,
 
-      {
-          path: '/profile',
-          name: 'Profile',
-          component: Profile
-      },
+        },
+            {
+                path: '/search',
+                name: 'Search',
+                component: Search
+            },
+            {
+                path: '/order',
+                name: 'Order',
+                component: Order
+            },
+
+            {
+                path: '/profile',
+                name: 'Profile',
+                component: Profile
+            },]
+    },
+
 
 
   ]

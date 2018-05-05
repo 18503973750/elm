c57d055fd8a5f4e8daa7c558bd10acd7037c3c5f
MiaoMiaoDeGit <m18838959446@163.com>
2018-05-05 14:51:10 +0800

    hhh

---
+++ elm/app/src/components/profile/DL.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="out">
+		
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "Profile",
+		
+	}
+</script>
+
+<style>
+	.out {
+		width: 100%;
+		height: 100%;
+		background: red;
+	}
+</style>

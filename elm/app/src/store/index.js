c75facd628a5e3dfffd1ded88f02bea715f32368
jae <626078563@qq.com>
2018-05-17 09:29:37 +0800

    1

--- elm/app/src/store/index.js
+++ elm/app/src/store/index.js
@@ -3,53 +3,40 @@ import Vue from "vue";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-    state: {
-<<<<<<< HEAD
-        isLogin:0, 
+    isLogin:0, 
+    order: '',
+    codes: []
+    st:'',
+    st1:'',
+    st2:'',
+    st3:''        
+},
+mutations: {
+    order(state, data) {
+        state.order = data;
     },
-    mutations: {
-	    changeLogin(state,data){
-	        state.isLogin = data;
-	    }
-=======
-        order: '',
+    codes(state, data) {
+      state.codes.push(data);
+  },
+  delete(state,index){
+   state.codes.splice(index,1)
+}
+},
+st(state,data){
+    state.st=data
 
-        codes: []
+},
+st1(state,data){
+    state.st1=data
+},
+st2(state,data){
+    state.st2=data
+    console.log(data+'!!!!')
 
-        st:'',
-        st1:'',
-        st2:'',
-        st3:''
-
-
-    },
-    mutations: {
-        order(state, data) {
-            state.order = data;
-        },
-        codes(state, data) {
-        		state.codes.push(data);
-        },
-        delete(state,index){
-        	state.codes.splice(index,1)
-        }
-        },
-        st(state,data){
-            state.st=data
-
-        },
-        st1(state,data){
-            state.st1=data
-        },
-        st2(state,data){
-            state.st2=data
-            console.log(data+'!!!!')
-
-        },
-        st3(state,data){
-            state.st3=data
-            console.log(data+'??????')
-        },
->>>>>>> 7d4fe0786e8e43006f3fd6c22acd7a0f4c0b7db0
-    }
+},
+st3(state,data){
+    state.st3=data
+    console.log(data+'??????')
+},
+}
 })

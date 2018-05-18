61e1c0e5f910c52cd13b4e60d7d9ef74c899e142
jae <626078563@qq.com>
2018-05-18 17:38:08 +0800

    123

--- elm/app/src/store/index.js
+++ elm/app/src/store/index.js
@@ -3,7 +3,6 @@ import Vue from "vue";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-<<<<<<< HEAD
     state: {
         isLogin: 0,
         order: '',
@@ -42,44 +41,5 @@ export default new Vuex.Store({
 
         },
 
-    },
-
-=======
->>>>>>> 4a93e8c42fbb772544433f29dd4959cc5e2e7132
-
-	state: {
-		isLogin: 0,
-		order: '',
-		codes: [],
-		st: '',
-		st1: '',
-		st2: '',
-		st3: '',
-	},
-	mutations: {
-		st(state, data) {
-			state.st = data
-
-		},
-		st1(state, data) {
-			state.st1 = data
-		},
-		st2(state, data) {
-			state.st2 = data
-			console.log(data + '!!!!')
-
-		},
-		st3(state, data) {
-			state.st3 = data
-			console.log(data + '??????')
-		},
-		//往address里添加列表
-		codes(state, data) {
-			state.codes.push(data);
-		},
-		//获取删除下标
-		delete(state, index) {
-			state.codes.splice(index, 1);
-		}
-	},
+    } 
 })

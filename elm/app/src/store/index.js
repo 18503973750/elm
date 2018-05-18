c56893ce258d73aef77ca19e3be7aeea75db370e
jae <626078563@qq.com>
2018-05-18 17:34:50 +0800

    11

---
+++ elm/app/src/store/index.js
@@ -0,0 +1,47 @@
+import Vuex from "vuex"
+import Vue from "vue";
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        isLogin: 0,
+        order: '',
+
+        st:'',
+        st1:'',
+        st3:'',
+        st4:'',
+
+
+
+        codes: [],
+
+
+    },
+    mutations: {
+
+        st(state,data){
+            state.st=data
+
+        },
+        st1(state,data){
+            state.st1=data
+
+        },
+        st3(state,data){
+            state.st3=data
+        },
+        st4(state,data){
+            state.st4=data
+        },
+
+
+        st2(state, data) {
+            state.st2 = data
+
+        },
+
+    },
+
+
+})
